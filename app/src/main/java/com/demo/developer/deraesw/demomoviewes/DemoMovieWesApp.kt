13953ab9feadf73fb927479fb6d15b32e57aa764
deraesw <deraes.wesley@gmail.com>
2021-05-23 22:09:22 -0700

    Remove Dagger from Application and replace with Hilt

--- app/src/main/java/com/demo/developer/deraesw/demomoviewes/DemoMovieWesApp.kt
+++ app/src/main/java/com/demo/developer/deraesw/demomoviewes/DemoMovieWesApp.kt
@@ -1,19 +1,7 @@
 package com.demo.developer.deraesw.demomoviewes
 
-import com.demo.developer.deraesw.demomoviewes.di.AppModule
-import com.demo.developer.deraesw.demomoviewes.di.DaggerAppComponent
-import dagger.android.AndroidInjector
-import dagger.android.DaggerApplication
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-
-class DemoMovieWesApp : DaggerApplication(){
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        return DaggerAppComponent
-                .builder()
-                .application(this)
-                .appModule(AppModule(this))
-                .build()
-    }
-
-
-}
+@HiltAndroidApp
+class DemoMovieWesApp : Application()
