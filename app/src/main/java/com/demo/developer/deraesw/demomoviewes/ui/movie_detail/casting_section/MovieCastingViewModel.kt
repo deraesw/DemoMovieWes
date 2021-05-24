da517c04935c892fec28bd9302f5b5ce6139eddf
deraesw <deraes.wesley@gmail.com>
2021-05-23 22:10:50 -0700

    Replace Dagger with Hilt inside ViewModels

--- app/src/main/java/com/demo/developer/deraesw/demomoviewes/ui/movie_detail/casting_section/MovieCastingViewModel.kt
+++ app/src/main/java/com/demo/developer/deraesw/demomoviewes/ui/movie_detail/casting_section/MovieCastingViewModel.kt
@@ -1,24 +1,25 @@
 package com.demo.developer.deraesw.demomoviewes.ui.movie_detail.casting_section
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.demo.developer.deraesw.demomoviewes.data.model.CastingItem
 import com.demo.developer.deraesw.demomoviewes.repository.MovieCreditsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MovieCastingViewModel
-@Inject constructor(
-        private val movieCreditsRepository: MovieCreditsRepository) : ViewModel() {
+@HiltViewModel
+class MovieCastingViewModel @Inject constructor(
+    private val movieCreditsRepository: MovieCreditsRepository
+) : ViewModel() {
 
     val errorNetwork = movieCreditsRepository.errorNetwork
 
     fun getMovieCasting(movieId: Int) = movieCreditsRepository.getCastingFromMovie(movieId)
 
-    fun getLimitedMovieCasting(movieId: Int, limit : Int) = movieCreditsRepository.getLimitedCastingFromMovie(movieId, limit)
+    fun getLimitedMovieCasting(movieId: Int, limit: Int) =
+        movieCreditsRepository.getLimitedCastingFromMovie(movieId, limit)
 
-    fun fetchMovieCredits(movieId : Int) {
+    fun fetchMovieCredits(movieId: Int) {
         viewModelScope.launch {
             movieCreditsRepository.fetchAndSaveMovieCredits(movieId)
         }
--- app/src/main/java/com/demo/developer/deraesw/demomoviewes/ui/movies_in_theater/MoviesInTheaterViewModel.kt
+++ app/src/main/java/com/demo/developer/deraesw/demomoviewes/ui/movies_in_theater/MoviesInTheaterViewModel.kt
@@ -7,20 +7,23 @@ import com.demo.developer.deraesw.demomoviewes.data.entity.MovieGenre
 import com.demo.developer.deraesw.demomoviewes.data.model.MovieInTheater
 import com.demo.developer.deraesw.demomoviewes.repository.MovieGenreRepository
 import com.demo.developer.deraesw.demomoviewes.repository.MovieRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MoviesInTheaterViewModel
-@Inject constructor(
-        private val movieRepository: MovieRepository,
-        private val movieGenreRepository : MovieGenreRepository) : ViewModel() {
+@HiltViewModel
+class MoviesInTheaterViewModel @Inject constructor(
+    private val movieRepository: MovieRepository,
+    private val movieGenreRepository: MovieGenreRepository
+) : ViewModel() {
 
-    val movieList : LiveData<List<MovieInTheater>> = movieRepository.moviesInTheater
-    val movieInTheaterWithGender : LiveData<List<MovieInTheater>> = movieRepository.mMovieInTheaterWithGenres
-    val movieGenre : LiveData<List<MovieGenre>> = movieGenreRepository.mMovieGenreList
+    val movieList: LiveData<List<MovieInTheater>> = movieRepository.moviesInTheater
+    val movieInTheaterWithGender: LiveData<List<MovieInTheater>> =
+        movieRepository.mMovieInTheaterWithGenres
+    val movieGenre: LiveData<List<MovieGenre>> = movieGenreRepository.mMovieGenreList
     val errorMessage = movieRepository.errorMessage
 
-    fun populateMovieInTheaterWithGenre(list: List<MovieInTheater>){
+    fun populateMovieInTheaterWithGenre(list: List<MovieInTheater>) {
         movieRepository.populateMovieInTheaterWithGenre(list)
     }
 
