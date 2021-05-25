224e9b68205a1bb7c747cba75b08c363f7ddab8d
deraesw <deraes.wesley@gmail.com>
2021-05-24 17:12:14 -0700

    Reformat appDatabase

--- app/src/main/java/com/demo/developer/deraesw/demomoviewes/data/appDatabase.kt
+++ app/src/main/java/com/demo/developer/deraesw/demomoviewes/data/appDatabase.kt
@@ -7,27 +7,29 @@ import androidx.room.RoomDatabase
 import com.demo.developer.deraesw.demomoviewes.data.dao.*
 import com.demo.developer.deraesw.demomoviewes.data.entity.*
 
-@Database(entities = [
-            (MovieGenre::class),
-            (Movie::class),
-            (MovieToGenre::class),
-            (People::class),
-            (Crew::class),
-            (Casting::class),
-            (ProductionCompany::class),
-            (MovieToProduction::class)
-        ],
-        version = 7)
-abstract class appDatabase : RoomDatabase(){
+@Database(
+    entities = [
+        (MovieGenre::class),
+        (Movie::class),
+        (MovieToGenre::class),
+        (People::class),
+        (Crew::class),
+        (Casting::class),
+        (ProductionCompany::class),
+        (MovieToProduction::class)
+    ],
+    version = 7
+)
+abstract class appDatabase : RoomDatabase() {
 
-    abstract fun movieGenreDao() : MovieGenreDAO
-    abstract fun movieDAO() : MovieDAO
-    abstract fun movieToGenreDAO() : MovieToGenreDAO
-    abstract fun peopleDAO() : PeopleDAO
-    abstract fun castingDAO() : CastingDAO
-    abstract fun crewDAO() : CrewDAO
-    abstract fun productionCompanyDao() : ProductionCompanyDao
-    abstract fun movieToProductionDao() : MovieToProductionDao
+    abstract fun movieGenreDao(): MovieGenreDAO
+    abstract fun movieDAO(): MovieDAO
+    abstract fun movieToGenreDAO(): MovieToGenreDAO
+    abstract fun peopleDAO(): PeopleDAO
+    abstract fun castingDAO(): CastingDAO
+    abstract fun crewDAO(): CrewDAO
+    abstract fun productionCompanyDao(): ProductionCompanyDao
+    abstract fun movieToProductionDao(): MovieToProductionDao
 
     override fun clearAllTables() {
         //todo see what to implement here
@@ -36,7 +38,8 @@ abstract class appDatabase : RoomDatabase(){
     companion object {
         private const val NAME_DATABASE = "demo_movie_wes.db"
 
-        @Volatile private var sInstance : appDatabase? = null
+        @Volatile
+        private var sInstance: appDatabase? = null
 
         fun getInstance(context: Context): appDatabase {
             sInstance ?: synchronized(this) {
@@ -49,12 +52,13 @@ abstract class appDatabase : RoomDatabase(){
         }
 
         private fun buildDatabase(context: Context) =
-                Room.databaseBuilder(
-                        context.applicationContext,
-                        appDatabase::class.java,
-                        appDatabase.NAME_DATABASE)
-                        .fallbackToDestructiveMigration()
-                        .build()
+            Room.databaseBuilder(
+                context.applicationContext,
+                appDatabase::class.java,
+                NAME_DATABASE
+            )
+                .fallbackToDestructiveMigration()
+                .build()
 
     }
 }
