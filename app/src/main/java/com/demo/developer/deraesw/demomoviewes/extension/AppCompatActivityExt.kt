2f3e0fdf0d6d8d50bbeb466be407128b5f285104
deraesw <deraes.wesley@gmail.com>
2021-06-04 21:20:45 -0700

    Clean up extension folder, remove unused extensions

--- app/src/main/java/com/demo/developer/deraesw/demomoviewes/extension/AppCompatActivityExt.kt
+++ app/src/main/java/com/demo/developer/deraesw/demomoviewes/extension/AppCompatActivityExt.kt
@@ -1,17 +1,8 @@
 package com.demo.developer.deraesw.demomoviewes.extension
 
-import androidx.fragment.app.Fragment
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 fun AppCompatActivity.showShortToast(message : String){
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
-
-fun AppCompatActivity.addFragmentToActivity(container : Int, fragment: androidx.fragment.app.Fragment) {
-    supportFragmentManager.beginTransaction().add(container, fragment).commit()
-}
-
-fun AppCompatActivity.replaceFragmentToActivity(container : Int, fragment: androidx.fragment.app.Fragment) {
-    supportFragmentManager.beginTransaction().replace(container, fragment).commit()
 }
